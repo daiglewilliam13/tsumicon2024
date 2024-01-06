4c3ece125c7f7252025f742253ada7eae577f9b8
William Daigle <daiglewilliam13@gmail.com>
2024-01-05 18:05:15 -0800

    add play on mouseover

--- js/guests.js
+++ js/guests.js
@@ -1,8 +1,14 @@
 console.log('connected guests.js')
 
-let card = document.getElementsByClassName('guest-card-wrapper')[0];
-console.log(card)
+let cards = Array.from(document.getElementsByClassName('guest-card-wrapper'));
 
-card.addEventListener('mouseover', function () {
-    console.log('mouse in')
+
+cards.forEach((card) => {
+    let video = card.children[2];
+    card.addEventListener('mouseover', function () {
+        video.play();
+    })
+    card.addEventListener('mouseout', function () {
+        video.pause();
+    })
 })
