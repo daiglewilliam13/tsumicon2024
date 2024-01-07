e7bebd8512606412e7777e8880e8423d2b8160d3
William Daigle <daiglewilliam13@gmail.com>
2024-01-06 21:09:09 -0800

    add guest list selector and animations for guest cards

--- js/guests.js
+++ js/guests.js
@@ -1,6 +1,9 @@
 console.log('connected guests.js')
 
 let cards = Array.from(document.getElementsByClassName('guest-card-wrapper'));
+let selectors = Array.from(document.getElementById('guest-selectors').children);
+let sections = Array.from(document.getElementsByClassName('guest-dropdown'));
+console.log(selectors);
 
 
 cards.forEach((card) => {
@@ -11,4 +14,50 @@ cards.forEach((card) => {
     card.addEventListener('mouseout', function () {
         video.pause();
     })
+})
+
+const highlightSelector = (value) => {
+    selectors.forEach((el) => {
+        let button = el
+        if (button.dataset.select == value) {
+            button.classList.add('selected')
+        } else {
+            button.classList.remove('selected');
+        }
+    })
+}
+
+const showSection = (value) => {
+
+    sections.forEach((div) => {
+        if (div.id == value) {
+            div.classList.add('show');
+        } else {
+            div.classList.remove('show');
+        }
+    })
+    let delay = 0;
+    cards.forEach((card) => {
+        let cardParent = card.parentElement.parentElement.id
+        if (cardParent == value) {
+            setTimeout(() => {
+                card.classList.add('fade-in')
+
+            }, delay)
+            delay = delay + 100;
+        } else {
+            card.classList.remove('fade-in')
+        }
+    })
+}
+selectors.forEach((el) => {
+    //figure out which button was pressed
+    el.addEventListener('click', function () {
+        highlightSelector(this.dataset.select)
+        showSection(this.dataset.select);
+    })
+    //go through each button and if the data-selector doesn't match pressed button, remove class
+
+    //cycle through divs and change from show to hide
+
 })
