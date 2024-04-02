fba99ce40e1b0ea0f679fce50d53fc25142a2aaa
William Daigle <daiglewilliam13@gmail.com>
2024-04-02 14:45:03 -0700

    update guest details and vendors

--- js/vendors.js
+++ js/vendors.js
@@ -131,122 +131,128 @@ export let artists = [
 ]
 
 export let vendors =
-    [
-        {
-            vendor: "The kgu Stop / What's Clackin",
-            booth: 65
-        },
-        {
-            vendor: 'Secrettsumori',
-            booth: 22,
-        },
-        {
-            vendor: 'Bishoujucomplex',
-            booth: 21,
-        }, {
-            vendor: 'catchmydrift.jp',
-            booth: 56,
-        }, {
-            vendor: 'mdotrage',
-            booth: 10,
-        }, {
-            vendor: 'angy frog art',
-            booth: 51,
-        }, {
-            vendor: 'basedkawaii',
-            booth: 3,
-        }, {
-            vendor: 'FreshTango',
-            booth: 29,
-        }, {
-            vendor: 'Send Calamity',
-            booth: 68,
-        }, {
-            vendor: 'ara.culture',
-            booth: 'On Map',
-        }, {
-            vendor: 'Buki Pins',
-            booth: 48,
-        }, {
-            vendor: '_Crisisculture',
-            booth: 12,
-        }, {
-            vendor: 'AnimeCoast',
-            booth: 67,
-        }, {
-            vendor: 'Dreamscape.cc',
-            booth: 69,
-        }, {
-            vendor: 'u.k.o fitness',
-            booth: 41,
-        }, {
-            vendor: 'Mangahole',
-            booth: 23,
-        }, {
-            vendor: 'Chibizilla',
-            booth: 52,
-        }, {
-            vendor: 'nekomimi punks',
-            booth: 49,
-        }, {
-            vendor: 'fur_Smith',
-            booth: 7,
-        }, {
-            vendor: '_Shinobu.us',
-            booth: 58,
-        }, {
-            vendor: 'Himawarii.US',
-            booth: 53,
-        }, {
-            vendor: 'iymagic',
-            booth: 11,
-        }, {
-            vendor: 'kurozoku',
-            booth: 55,
-        }, {
-            vendor: 'Pikadude',
-            booth: 74,
-        }, {
-            vendor: 'Low Standards Designs ',
-            booth: 66,
-        }, {
-            vendor: 'WNTD INC',
-            booth: 46,
-        }, {
-            vendor: 'V1Tech',
-            booth: 'On Map',
-        }, {
-            vendor: 'Senpai Squad',
-            booth: 'On Map',
-        }, {
-            vendor: 'Xpresskins',
-            booth: 'On Map',
-        }, {
-            vendor: 'Otakuruma',
-            booth: 'On Map',
-        }, {
-            vendor: 'Furescent',
-            booth: 6,
-        }, {
-            vendor: 'Pandora X',
-            booth: 50,
-        }, {
-            vendor: 'Bimyou Workshop',
-            booth: 72,
-        }, {
-            vendor: 'Kusoya.jp',
-            booth: 61,
-        }, {
-            vendor: 'hiromu.designs',
-            booth: 2,
-        },
-        {
-            vendor: 'Tenga',
-            booth: 40,
-        }, {
-            vendor: 'Weebculture',
-            booth: 8,
-        }
+    [{
+        vendor: 'MEH Clothing',
+        booth: 5
+    }, {
+        vendor: 'KimochiLabs',
+        booth: 4
+    },
+    {
+        vendor: "The kgu Stop / What's Clackin",
+        booth: 65
+    },
+    {
+        vendor: 'Secrettsumori',
+        booth: 22,
+    },
+    {
+        vendor: 'Bishoujucomplex',
+        booth: 21,
+    }, {
+        vendor: 'catchmydrift.jp',
+        booth: 56,
+    }, {
+        vendor: 'mdotrage',
+        booth: 10,
+    }, {
+        vendor: 'angy frog art',
+        booth: 51,
+    }, {
+        vendor: 'basedkawaii',
+        booth: 3,
+    }, {
+        vendor: 'FreshTango',
+        booth: 29,
+    }, {
+        vendor: 'Send Calamity',
+        booth: 68,
+    }, {
+        vendor: 'ara.culture',
+        booth: 'On Map',
+    }, {
+        vendor: 'Buki Pins',
+        booth: 48,
+    }, {
+        vendor: '_Crisisculture',
+        booth: 12,
+    }, {
+        vendor: 'AnimeCoast',
+        booth: 67,
+    }, {
+        vendor: 'Dreamscape.cc',
+        booth: 69,
+    }, {
+        vendor: 'u.k.o fitness',
+        booth: 41,
+    }, {
+        vendor: 'Mangahole',
+        booth: 23,
+    }, {
+        vendor: 'Chibizilla',
+        booth: 52,
+    }, {
+        vendor: 'nekomimi punks',
+        booth: 49,
+    }, {
+        vendor: 'fur_Smith',
+        booth: 7,
+    }, {
+        vendor: '_Shinobu.us',
+        booth: 58,
+    }, {
+        vendor: 'Himawarii.US',
+        booth: 53,
+    }, {
+        vendor: 'iymagic',
+        booth: 11,
+    }, {
+        vendor: 'kurozoku',
+        booth: 55,
+    }, {
+        vendor: 'Pikadude',
+        booth: 74,
+    }, {
+        vendor: 'Low Standards Designs ',
+        booth: 66,
+    }, {
+        vendor: 'WNTD INC',
+        booth: 46,
+    }, {
+        vendor: 'V1Tech',
+        booth: 'On Map',
+    }, {
+        vendor: 'Senpai Squad',
+        booth: 'On Map',
+    }, {
+        vendor: 'Xpresskins',
+        booth: 'On Map',
+    }, {
+        vendor: 'Otakuruma',
+        booth: 'On Map',
+    }, {
+        vendor: 'Furescent',
+        booth: 6,
+    }, {
+        vendor: 'Pandora X',
+        booth: 50,
+    }, {
+        vendor: 'Bimyou Workshop',
+        booth: 72,
+    }, {
+        vendor: 'Kusoya.jp',
+        booth: 61,
+    }, {
+        vendor: 'hiromu.designs',
+        booth: 2,
+    },
+    {
+        vendor: 'Tenga',
+        booth: 40,
+    }, {
+        vendor: 'Weebculture',
+        booth: 8,
+    }
     ]
 
 export let artistAlley = [
